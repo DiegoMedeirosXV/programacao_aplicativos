94b007c39d057eec8792eff44a9540f0d9c66670
DiegoMedeirosXV <diegomedeiros7171@gmail.com>
2025-07-28 09:42:40 -0300

    AAA

---
+++ aula0.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Digite o seu nome: ");
+
+let idade = prompt("Digite seu idade: ")
+
+   console.log("Bem Vindo,", nome, "sua idade é", idade, "anos!" );
+   
